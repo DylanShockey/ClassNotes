44fd07916e4e3097762459bf01275bdf5a28a4d7
Dylan Shockey <shockeyd@student.swosu.edu>
2015-10-07 09:48:32 -0500

    Added an outline

--- src/ClassNotesProject1.java
+++ src/ClassNotesProject1.java
@@ -3,6 +3,25 @@ public class ClassNotesProject1 {
 	
 		System.out.println("Hello");
 		
+		//Outline for addition problem.
+		
+		//Round 1
+		// Generate 2 random numbers
+		// Ask the user to add these two numbers together
+		// Read in their response
+		// Check if the answer was correct
+		//  IF correct
+		//     Tell them it was correct
+		//     Give them points
+		//     Make the next question harder
+		//  IF not correct
+		//     Tell them it was wrong
+		//     Tell them the correct answer
+		//     Do not give them points
+		//     Make the next question easier
+		//Round 2
+		//Round 3
+		//Round 4
 	}
 
 }
