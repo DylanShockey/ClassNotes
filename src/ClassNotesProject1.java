dd07bfaedbea91bfe0879a41f9ea0a8dbd432efe
Dylan Shockey <shockeyd@student.swosu.edu>
2015-10-12 09:48:13 -0500

    added material for round 1

--- src/ClassNotesProject1.java
+++ src/ClassNotesProject1.java
@@ -5,20 +5,38 @@ public class ClassNotesProject1 {
 		
 		//Outline for addition problem.
 		
-		//Round 1
-		// Generate 2 random numbers
-		// Ask the user to add these two numbers together
-		// Read in their response
-		// Check if the answer was correct
-		//  IF correct
-		//     Tell them it was correct
-		//     Give them points
-		//     Make the next question harder
-		//  IF not correct
-		//     Tell them it was wrong
-		//     Tell them the correct answer
-		//     Do not give them points
-		//     Make the next question easier
+		// Round 1
+		//	Generate 2 random numbers
+		int number1 = (int)(Math.random()*10);
+		System.out.println("Random number 1 is: " + number1);
+		int number2 = (int)(Math.random()*10);
+		System.out.println("Random number 2 is: " + number2);
+		
+		int correctAnswer = number1 + number2;
+		//  Ask the user to add these two numbers together
+		System.out.println("What is " + number1 + " + " + number2 +" = ?");
+
+		//  Read in their response
+		Scanner input = new Scanner(System.in);
+		int studentAnswer = input.nextInt();
+
+		//  Check if the answer was correct
+		if(correctAnswer == studentAnswer){
+			//		IF correct
+			//			Tell them it was correct
+			System.out.println("Your answer was correct");
+			//			Give them points
+			//			Make the next question harder
+			
+		}else{
+			//		IF not correct
+			//			Tell them it was wrong
+			System.out.println("Your answer was not correct");
+			// 			Tell them the correct answer
+			//			Do not give them points
+			//			Make the next question easier
+			
+		}
 		//Round 2
 		// Generate 2 random numbers
 		// Ask the user to add these two numbers together
